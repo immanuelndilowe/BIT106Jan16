8bc6f4ef158fa11d75940ee4a74ca87826de4309
shoegit <ngsm@help.edu.my>
2016-01-12 09:34:55 +0800

    Introductory Program

---
+++ BIT106Jan16/src/HelloBIT106.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * This is my introductory program
+ * @author ngsm
+ */
+public class HelloBIT106 {
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Hello BIT106!");
+    }
+}
